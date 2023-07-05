bedc4abe7eaf7212b3c6fbc52fcf2aff5822548c
Kevin Bourgitteau <kevin.bourgitteau@gmail.com>
2023-07-05 12:37:40 +0200

    add script

--- js/app.js
+++ js/app.js
@@ -1,1 +1,38 @@
+const starContainer = document.querySelector('.star-container');
+const stars = document.querySelectorAll('.star');
+const commentBox = document.querySelector('.comment-container');
+const comments = [
+	"A chier",
+	"Bof",
+	"C'est juste",
+	"On kiffe",
+	"C'est du génie"
+]
+
+stars.forEach((star, index) => {
+    star.setAttribute("id", index + 1);
+
+    star.addEventListener("mouseover", () => {
+        stars.forEach((star, i) => {
+            if (i <= index) {
+                star.style.fill = "#F3F4F6";
+                star.querySelector("path").setAttribute("stroke", "#F3F4F6");
+								commentBox.innerHTML = comments[i];
+								
+            } else {
+                star.style.fill = "";
+                star.querySelector("path").setAttribute("stroke", "#4B5563");
+            }
+        });
+    });
+
+    starContainer.addEventListener("mouseout", () => {
+        stars.forEach((star, i) => {
+            if (i > index) {
+                star.style.fill = "";
+                star.querySelector("path").setAttribute("stroke", "#4B5563");
+            }
+        });
+    });
+});
 
